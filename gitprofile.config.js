014d3e409ddc1c8f9ba80efbd91e55d2118d137f
Mitchell Urgero <info@urgero.org>
2023-07-22 01:44:02 -0500

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -4,7 +4,7 @@ const config = {
   github: {
     username: 'mitchellurgero', // Your GitHub org/user name. (Required)
     sortBy: 'updated', // stars | updated
-    limit: 5, // How many projects to display.
+    limit: 6, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
       projects: ['lotus-bot','mitchellurgero.github.io'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
